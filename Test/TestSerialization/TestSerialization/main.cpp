aa28aaf5354b578951565aa0078732dd69aa6a3e
fmw <3386358204@qq.com>
2022-11-15 08:28:21 +0800

    serialization

---
+++ Test/TestSerialization/TestSerialization/main.cpp
@@ -0,0 +1,23 @@
+﻿#include "MainWindow.h"
+
+#include <QApplication>
+#if _MSC_VER >= 1600    //解决msvc中文乱码的问题
+#pragma execution_character_set("utf-8")
+#endif
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+//    MainWindow w;
+
+
+//    w.show();
+    QFile file("F://Code//Qt//Test//TestSerialization//TestSerialization");
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QDataStream out(&file);
+    out << QString("王老击");
+    file.flush();
+    file.close();
+
+
+    return a.exec();
+}
